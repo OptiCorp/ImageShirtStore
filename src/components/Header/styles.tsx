51317899329ac226ae171124519fd2bd5a5e503b
Kathrine <kathrine.kodehode@gmail.com>
2023-04-09 22:52:05 +0200

    basic styling and Route Links

---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import Shirt from '../../images/36634-6-blank-white-t-shirt-template.png';
+
+export const Wrapper = styled.div`
+    padding: 10px;
+    margin: 0 auto;
+    max-height: 10rem;
+    background-color: #4f85b5;
+`;
+
+export const Links = styled.h1`
+    width: 50%;
+    margin: 2% auto;
+    display: flex;
+    list-style-type: none;
+    text-decoration: none;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 0.8rem;
+`;
