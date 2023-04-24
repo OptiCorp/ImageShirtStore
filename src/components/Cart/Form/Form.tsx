bfe1589ab28b9c9273bb7603d875f7612f49f219
Kathrine <kathrine.kodehode@gmail.com>
2023-04-24 17:05:49 +0200

    perf: :art: added better Cart modal component, and checkout Form styles fixed

--- src/components/Cart/Form/Form.tsx
+++ src/components/Cart/Form/Form.tsx
@@ -3,17 +3,10 @@ import { useDispatch } from 'react-redux';
 import { FunctionComponent, useState, useEffect } from 'react';
 import { useAppSelector } from '../../../hooks/hooks';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { Accordion } from '@equinor/eds-core-react';
+import { clearCart } from '../cartSlice';
 
-import { removeItem, decrementQuantity, incrementQuantity, clearCart } from '../cartSlice';
-
-import {
-	ModalBoxCheckout,
-	ModaloverlayCheckout,
-	CloseButtonStyled,
-	CardPreview,
-	NumbermodalCart,
-	CardButton,
-} from './Modulstyles';
+import { Dialog } from '@equinor/eds-core-react';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 import {
@@ -24,8 +17,14 @@ import {
 	SubmitInput,
 	StyledInput,
 	Number,
-	CartButton,
+	SpanFieldContainer,
+	SpanField1,
+	SpanField2,
+	SpanField3,
+	SpanField4,
 } from './styles';
+
+import { HeaderAcc, PanelAcc } from './Accordionstyles';
 
 export const getTotal = () => {
 	const cart = useAppSelector(state => state.reducer.cart.items);
@@ -48,9 +47,7 @@ export const getTotalQuantity = () => {
 };
 
 interface ModalForm {
-	children?: ReactNode;
-	isOpen: boolean;
-	toggle: () => void;
+	expanded: boolean;
 }
 type FormValues = {
 	firstName: string;
@@ -64,57 +61,74 @@ type FormValues = {
 	password: { value: number };
 };
 
-export const Form: FunctionComponent<ModalForm> = ({ toggle, isOpen }) => {
+export const Form: FunctionComponent<ModalForm> = () => {
 	const dispatch = useDispatch();
 
 	const { register, handleSubmit, reset } = useForm<FormValues>();
 	const onSubmit = handleSubmit((data: FormValues) => console.log(data));
+	const [expanded, setExpanded] = useState(false);
+	const toggleAccordion = (state: boolean) => {
+		console.log(state);
+		setExpanded(state);
+	};
 
 	return (
-		<div>
-			{isOpen && (
-				<ModaloverlayCheckout onClick={toggle}>
-					<CloseButtonStyled onClick={toggle}>
-						<AiOutlineCloseCircle size={50} />
-					</CloseButtonStyled>
-					<ModalBoxCheckout>
-						<FormWrapper>
-							<Summary>
-								<Number>
-									total ({getTotal().totalQuantity} items) :{' '}
-									<strong>${getTotal().totalPrice}</strong>
-								</Number>
-							</Summary>
-							<FormStyle onSubmit={onSubmit}>
-								<StyledLabel>Email</StyledLabel>
-								<StyledInput type='email' {...register('email')} />
-								<StyledLabel>First Name</StyledLabel>
-								<StyledInput {...register('firstName')} />
-								<StyledLabel>Last Name</StyledLabel>
-								<StyledInput {...register('lastName')} />
-								<StyledLabel>Street</StyledLabel>
-								<StyledInput {...register('address')} />
-								<StyledLabel>City</StyledLabel>
-								<StyledInput {...register('city')} />
-								<StyledLabel>State</StyledLabel>
-								<StyledInput {...register('state')} />
-								<StyledLabel>Zip</StyledLabel>
-								<StyledInput {...register('zipcode')} />
-								<StyledLabel>Country</StyledLabel>
-								<StyledInput {...register('country')} />
-								<StyledLabel>Credit Card information</StyledLabel>
-								<StyledInput type='password' {...register('password')} />
-								<StyledLabel>EXP</StyledLabel>
-								<StyledInput type='password' {...register('password')} />
-								<StyledLabel>CCV</StyledLabel>
-								<StyledInput type='password' {...register('password')} />
-
-								<SubmitInput type='submit' onClick={() => dispatch(clearCart())} />
-							</FormStyle>
-						</FormWrapper>
-					</ModalBoxCheckout>
-				</ModaloverlayCheckout>
-			)}
-		</div>
+		<FormWrapper>
+			<button
+				onClick={() => toggleAccordion(!expanded)}
+				style={{ width: 'fit-content' }}
+			></button>
+			<Accordion>
+				<Accordion.Item isExpanded={expanded} onExpandedChange={toggleAccordion}>
+					<HeaderAcc>Checkout</HeaderAcc>
+					<PanelAcc>
+						<Summary>
+							<Number>
+								total ({getTotal().totalQuantity} items) :{' '}
+								<strong>${getTotal().totalPrice}</strong>
+							</Number>
+						</Summary>
+						<FormStyle onSubmit={onSubmit}>
+							<SpanFieldContainer>
+								<SpanField1>
+									{' '}
+									<StyledLabel>Email</StyledLabel>{' '}
+									<StyledInput type='email' {...register('email')} />{' '}
+									<StyledLabel>First Name</StyledLabel>
+									<StyledInput {...register('firstName')} />
+									<StyledLabel>Last Name</StyledLabel>
+									<StyledInput {...register('lastName')} />{' '}
+								</SpanField1>
+								<SpanField2>
+									<StyledLabel>Street</StyledLabel>
+									<StyledInput {...register('address')} />
+									<StyledLabel>City</StyledLabel>
+									<StyledInput {...register('city')} />
+									<StyledLabel>State</StyledLabel>
+									<StyledInput {...register('state')} />
+								</SpanField2>
+								<SpanField3>
+									<StyledLabel>Zip</StyledLabel>
+									<StyledInput {...register('zipcode')} />
+									<StyledLabel>Country</StyledLabel>
+									<StyledInput {...register('country')} />
+								</SpanField3>
+								<SpanField4>
+									<StyledLabel>Credit Card information</StyledLabel>
+									<StyledInput type='password' {...register('password')} />
+									<StyledLabel>EXP</StyledLabel>
+									<StyledInput type='password' {...register('password')} />
+									<StyledLabel>CCV</StyledLabel>
+									<StyledInput type='password' {...register('password')} />
+								</SpanField4>{' '}
+								<SubmitInput type='submit' onClick={() => dispatch(clearCart())} />{' '}
+							</SpanFieldContainer>
+						</FormStyle>
+					</PanelAcc>
+				</Accordion.Item>
+			</Accordion>
+		</FormWrapper>
+		// 	</Test>
+		// </Test2>
 	);
 };
